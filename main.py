aa1f5693d76000d739f78346734588690653c207
Alex Bubnov <avb_it@outlook.com>
2022-10-14 23:10:07 +0500

    re with cicle

--- main.py
+++ main.py
@@ -3,14 +3,14 @@ import re
 
 def process(s):
     words = re.split(r' ', s)
-    for i, word in enumerate(words):
+    for i in range(0, len(words))
         if i < len(words) - 1 and words[i] == words[i+1]:
             words.pop(i)
     print(words)
 
 
 def main():
-    for i in range(0, 6):
+    for i in range(1, 6):
         with open(f'2test{i}.txt') as f:
             process(f.read())
 
