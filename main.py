1f5611acbb1ac85b42d4177d589761804489d961
Alex Bubnov <avb_it@outlook.com>
2022-10-14 22:28:42 +0500

    re split

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ s = 'qweqw ewq sad arq arq das rrq3 awae awae q rqawf wefq fwf ae fwwwf'
 def main():
     patern = re.compile(r' ')
     words = patern.split(s)
+    print(words)
     for i, word in enumerate(words):
         print(f'{i} {word}')
 
