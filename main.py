45f796de99dec7b9e8a3efaeca8bbd01966911e2
Alex Bubnov <avb_it@outlook.com>
2022-10-14 23:38:50 +0500

    re with cicle

--- main.py
+++ main.py
@@ -2,15 +2,17 @@ import re
 
 
 def process(s):
-    words = re.split(r' ', s)
+    words = re.split(r'\s', s)
     for i in range(0, len(words)):
-        if i < len(words) - 1 and words[i] == words[i+1]:
-            words.pop(i)
+        if i < len(words) - 1:
+            patern = f'^{words[i]}[.,?]*$'
+            if re.match(patern, words[i+1]):
+                words.pop(i)
     print(words)
 
 
 def main():
-    for i in range(1, 6):
+    for i in range(1, 2):
         with open(f'2test{i}.txt') as f:
             process(f.read())
 
