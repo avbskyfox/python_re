eb7bbe5a557927503d6402d3463db2927c69fbf8
Alex Bubnov <avb_it@outlook.com>
2022-10-14 22:26:23 +0500

    re split

---
+++ main.py
@@ -0,0 +1,14 @@
+import re
+
+s = 'qweqw ewq sad arq arq das rrq3 awae awae q rqawf wefq fwf ae fwwwf'
+
+
+def main():
+    patern = re.compile(r' ')
+    words = patern.split(s)
+    for i, word in enumerate(words):
+        print(f'{i} {word}')
+
+
+if __name__ == '__main__':
+    main()
